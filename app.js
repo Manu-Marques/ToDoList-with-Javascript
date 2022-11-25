dfd226af597eb04d7c2b972f3fb7cfa30539f65c
Manu <le.marques.emanuel@gmail.com>
2022-11-25 16:29:09 +0100

    javascript added

--- app.js
+++ app.js
@@ -18,10 +18,73 @@ window.addEventListener('load', () => {
 
         const task_content_el = document.createElement('div');
         task_content_el.classList.add('content');
-        task_content_el.innerText = task;
 
         task_el.appendChild(task_content_el);
 
+        const task_input_el = document.createElement('input');
+        task_input_el.classList.add('text');
+        task_input_el.value = task;
+        task_input_el.type = 'text';
+        task_input_el.setAttribute('readonly', 'readonly');
+
+        task_content_el.appendChild(task_input_el);
+
+        const task_actions_el = document.createElement('div');
+        task_actions_el.classList.add('actions');
+
+        const task_edit_el = document.createElement('button');
+        task_edit_el.classList.add('edit');
+        task_edit_el.innerHTML = 'Edit';
+
+        const task_delete_el = document.createElement('button');
+        task_delete_el.classList.add('delete');
+        task_delete_el.innerHTML = 'Delete';
+
+        const task_done_el = document.createElement('button');
+        task_done_el.classList.add('checkbox');
+        task_done_el.innerHTML = 'Done';
+
+        task_actions_el.appendChild(task_edit_el);
+        task_actions_el.appendChild(task_delete_el);
+        task_actions_el.appendChild(task_done_el);
+        
+        task_el.appendChild(task_actions_el);
+        
         list_el.appendChild(task_el);
-})
+
+        input.value = '';
+
+        task_done_el.checked = task.completed;
+
+        task_edit_el.addEventListener('click', () => {
+            if (task_edit_el.innerText.toLocaleLowerCase() === 'edit') {
+                task_input_el.focus();
+                task_input_el.removeAttribute('readonly');
+                task_edit_el.innerText = 'Save';
+            } else {
+                task_input_el.setAttribute('readonly', 'readonly');
+                task_edit_el.innerText = 'Edit';
+            }
+
+        });
+
+        task_delete_el.addEventListener('click', () => {
+            task_el.remove();
+        });
+
+        task_done_el.addEventListener('click', () => {
+            task_el.completed = !task_el.completed
+            finishedTask();
+        });
+        
+        function finishedTask() {
+            if (task_el.completed == true) {
+                task_el.classList.add('done')
+            }
+            else {
+                task_el.classList.remove('done')
+            }
+          }
+
+    })
 })
